bd3b8b6279eed6919f0f8f4cdbc4d8f749f66a0d
Adelino Afonso <adelino-afonso@hotmail.com>
2016-02-02 15:01:47 -0300

    Create intervalos.cpp

---
+++ intervalos.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+	
+	using namespace std;
+
+	int main(){
+
+		auto number( 0 );
+		auto cont  ( 0u );
+		auto cont1 ( 0u );
+		auto cont2 ( 0u );
+		auto cont3 ( 0u );
+		auto cont4 ( 0u );
+			
+			cout << "Entre com valores inteiros (Ctrl+d p/ encerrar): " << endl;
+			
+			while(cin >> number){
+				
+				if (number >=0 && number < 25) cont1++;
+				if (number >=25 && number < 50) cont2++;
+				if (number >=50 && number < 75) cont3++;				
+				if (number >=75 && number <= 100) cont4++;
+					cont++;
+			}
+
+		std::cout << static_cast<float> (cont1)/cont * 100 << "/100" << " no intervalo [0, 25)" << endl;
+		std::cout << static_cast<float> (cont2)/cont * 100 << "/100" << " no intervalo [25, 50)" << endl;
+		std::cout << static_cast<float> (cont3)/cont * 100 << "/100" << " no intervalo [50, 75)" << endl;
+		std::cout << static_cast<float> (cont4)/cont * 100 << "/100" << " no intervalo [75, 100]" << endl;
+
+		return EXIT_SUCCESS;
+	}

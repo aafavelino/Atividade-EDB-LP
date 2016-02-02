3ab8764c1443434f872fc45b31b320e0f2ea4496
Adelino Afonso <adelino-afonso@hotmail.com>
2016-02-02 13:11:09 -0300

    Create negativo5.cpp
    1. Escreva um programa em C/C++ chamado negativo5.cpp que lê 5 valores inteiros, um de cada vez, conta quantos destes valores são negativos e imprime esta informação.

---
+++ negativo5.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#define count 5
+
+using namespace std;
+	
+	int main(){
+		auto number ( 0 ); // Recebe o número
+		auto counter ( 0 ); // Contador para armazenar a quantidade de negativos
+		int i;
+
+		std::cout << ">>>Digite 5 números: ";
+
+		for (i = 0; i < count; ++i)
+		{
+			std:: cin >> number;
+
+			if(number < 0) 
+				counter ++;
+		}
+
+		std::cout << ">>>A Quantidade de números negativos foram " << counter << " de um total de " << count << " números digitados.";
+		
+		return EXIT_SUCCESS;
+	}

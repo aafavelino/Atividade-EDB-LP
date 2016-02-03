d66f9654e16505a979b0a6c0987178378780097c
Adelino Afonso <adelino-afonso@hotmail.com>
2016-02-03 13:31:26 -0300

    Create 07-troca seguintes vet.cpp

---
+++ 07-troca seguintes vet.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#define count 20
+
+	using namespace std;
+
+		int main(){
+
+			int vet[20];
+			int menor;
+			int posicao;
+			int impar;
+
+				for (int i = 0; i < count; ++i)
+				{
+					std::cin >> vet[i];
+				}
+
+				for (int i = 0; i < count; ++i)
+				{
+					std::cout << vet[i] << " ";
+					
+				} 
+
+
+				for (int i = 0; i <= count-1; i = i+2)
+				{
+						impar = vet[i];
+						vet[i] = vet[i+1];
+						vet[i+1] = impar;
+					
+				} 
+
+				std::cout << "\n";
+
+				for (int i = 0; i < count; ++i)
+				{
+					std::cout << vet[i] << " ";
+				}
+
+
+			return EXIT_SUCCESS;
+		}
